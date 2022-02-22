1abab9b8781202916996a3c4653435b5c5e34100
Riska5 <99956026+Riska5@users.noreply.github.com>
2022-02-22 17:55:47 +0700

    Day4_java_stdin_and_stdout_II.java

---
+++ Day4_java_stdin_and_stdout_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double g = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + g);
+        System.out.println("Int: " + a);
+    }
+}
